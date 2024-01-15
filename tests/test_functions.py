340bd598bdc166cf0dc3e85e567a81e5b2c7e59f
alenaodinets <alena.odinecz2013@yandex.ru>
2024-01-15 20:21:08 +0400

    functions_test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,5 @@
-from coursework.src.functions import modify_date, modify_from, modify_to
+from coursework.src.functions import format_operation, modify_date, modify_from, modify_to, sorted_list
+import pytest
 
 def test_modify_date():
     assert modify_date("2018-04-16T17:34:19.241289") == "16-04-2018"
@@ -9,8 +10,10 @@ def test_modify_from():
     assert modify_from("Счет 33355011456314142963") == "Счет **2963"
     assert modify_from("Visa Platinum 1813166339376336") == "Visa Platinum 1813 16** **** 6336"
     assert modify_from("Maestro 1913883747791351") == "Maestro 1913 88** **** 1351"
-    assert modify_from("") == "Открытие вклада"
+
 
 
 def test_modify_to():
     assert modify_to("Счет 33355011456314142963") == "Счет **2963"
+    assert modify_to("Счет 65778657373636465777") == "Счет **5777"
+
